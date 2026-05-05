bd74afba776bba8d4e35ca98fea35bcd112fc330
James Woolfenden <james.woolfenden@gmail.com>
2026-05-05 23:41:49 +0100

    fix docs

--- example/exampleb/module.store.tf
+++ example/exampleb/module.store.tf
@@ -1,11 +1,11 @@
 module "store" {
-  source           = "../../"
-  common_tags      = var.common_tags
-  bucket_name      = "guff"
-  project          = var.project
-  location         = var.location
-  binding_role     = "roles/storage.objectViewer"
-  binding_members  = ["user:james.woolfenden@gmail.com"]
-  log_bucket       = ""
-  force_destroy    = true
+  source          = "../../"
+  common_tags     = var.common_tags
+  bucket_name     = "guff"
+  project         = var.project
+  location        = var.location
+  binding_role    = "roles/storage.objectViewer"
+  binding_members = ["user:james.woolfenden@gmail.com"]
+  log_bucket      = ""
+  force_destroy   = true
 }
