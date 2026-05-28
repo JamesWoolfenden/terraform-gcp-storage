c356a9e327e062a7e93ed07ca7fc389a6508bc94
James Woolfenden <james.woolfenden@gmail.com>
2026-05-28 01:55:56 +0100

    chore: suppress HLD_TF_026 in example module blocks
    Examples intentionally use ../../ to reference the local module root.

---
+++ .pike/pike.generated_policy.tf
@@ -0,0 +1,15 @@
+
+resource "google_project_iam_custom_role" "terraform_pike" {
+  project     = "pike-477416"
+  role_id     = "terraform_pike"
+  title       = "terraform_pike"
+  description = "A user with least privileges"
+  permissions = [
+    "storage.buckets.create",
+    "storage.buckets.delete",
+    "storage.buckets.get",
+    "storage.buckets.getIamPolicy",
+    "storage.buckets.setIamPolicy",
+    "storage.buckets.update"
+  ]
+}
